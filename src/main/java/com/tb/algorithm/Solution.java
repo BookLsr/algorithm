4599c958fcbe145f71223b26d325f0f5ed08d371
windpursuer <1059641703@qq.com>
2020-03-05 22:25:40 +0800

    算法提交

---
+++ src/main/java/com/tb/algorithm/Solution.java
@@ -0,0 +1,36 @@
+package com.tb.algorithm;
+
+/**
+ * @Description:
+ * @Author: WindPursuer
+ * @Date 2020/3/5 9:54 PM
+ */
+
+/**
+ * 删除排序数组中的重复项,不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
+ */
+public class Solution {
+
+
+    public int removeDuplicates(int[] nums) {
+        /**
+         * 1、判断长度
+         * 2、进行相同前置操作
+         */
+        int length = nums.length;
+        if (length <= 1) {
+            return length;
+        }
+        int temp = 0;
+        for (int i = 1; i < length; i++) {
+            if (nums[i] != nums[i-1]) {
+                temp += 1;
+                nums[temp] = nums[i];
+            }
+
+
+        }
+        return temp + 1;
+
+    }
+}
