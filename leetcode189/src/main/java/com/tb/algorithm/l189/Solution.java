0a6c98655efb489179952f080d8647ee2c740c13
windpursuer <1059641703@qq.com>
2020-03-06 21:32:01 +0800

    算法提交

--- leetcode189/src/main/java/com/tb/algorithm/l189/Solution.java
+++ leetcode189/src/main/java/com/tb/algorithm/l189/Solution.java
@@ -14,33 +14,73 @@ public class Solution {
 
     public void rotate(int[] nums, int k) {
 
+            if (null == nums || nums.length <= 1 || k < 0) {
+                System.out.println("不符合条件");
+                return;
+            }
+
+            k = k % nums.length;
+            for (int i = 1; i <= k; i++) {
+
+                int temp = nums[0];
+                for (int j = 1; j < nums.length; j++) {
+                    if (j == nums.length - 1) {
+                        nums[0] = nums[j];
+                        nums[j] = temp;
+                        break;
+                    }
+                    int temp1 = nums[j];
+                    nums[j] = temp;
+                    temp = temp1;
+                }
+
+            }
+
+    }
+
+    /**
+     * 参考方法2， 举例 1 2 3 4 5 6   K=3
+     *  第一次循环、 1 -> 4, 4 -> 1,
+     *  第二次循环、 2 -> 5, 5 -> 2,
+     *  第三次循环、 3 -> 6, 6 -> 3,
+     *  完全替换
+     *
+     *  举例 1 2 3 4 5 6 7   K=3
+     *      *  第一次循环、 1 -> 4, 4 -> 7, 7 -> 3, 3 -> 6, 6 -> 2, 2 -> 5, 5 -> 1
+     *
+     * @param nums
+     * @param k
+     */
+    public void rotate2(int[] nums, int k) {
+
         if (null == nums || nums.length <= 1 || k < 0) {
             System.out.println("不符合条件");
             return;
         }
+        int count = 0;
+        for (int i = 0; count < nums.length; i++) {
+            int current = i;
+            int prev = nums[i];
+            do {
+                int next = (current + k) % nums.length;
+                int temp = nums[next];
+                nums[next] = prev;
+                prev = temp;
+                current = next;
+                count++;
+            } while (i != current);
 
-        k = k % nums.length;
-        for (int i = 1; i <= k; i++) {
-
-            int temp = nums[0];
-            for (int j = 1; j < nums.length; j++) {
-                if (j == nums.length - 1) {
-                    nums[0] = nums[j];
-                    nums[j] = temp;
-                    break;
-                }
-                int temp1 = nums[j];
-                nums[j] = temp;
-                temp = temp1;
-            }
 
         }
 
     }
 
+
+
     public static void main(String[] args) {
         Solution solution = new Solution();
         int[] a = {1, 2, 3, 4, 5, 6, 7};
-        solution.rotate(a, 3);
+        solution.rotate2(a, 3);
+        System.out.println(1);
     }
 }
